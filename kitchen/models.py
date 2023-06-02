25cc3452c1c2ec91df04da6c955607293841cc25
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2023-06-02 20:03:44 +0300

    Create model 'dish type''ingredients'

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredients(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
