8561e5f2f8f5925f50a8198b6baad6f1f3c28017
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2023-06-04 18:00:11 +0300

    Create dish-type CRUD view

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from .views import (
     index,
     DishTypeListView,
-    DishTypeDetailView,
-    DishTypeCreateView,
-    DishTypeUpdateView,
-    DishTypeDeleteView,
+    # DishTypeDetailView,
+    # DishTypeCreateView,
+    # DishTypeUpdateView,
+    # DishTypeDeleteView,
 )
 
 urlpatterns = [
@@ -14,18 +14,18 @@ urlpatterns = [
     path("dish_type/",
          DishTypeListView.as_view(),
          name="dish-type-list"),
-    path("dish_type/<int:pk>/detail/",
-         DishTypeDetailView.as_view(),
-         name="dish-type-detail"),
-    path("dish_type/create/",
-         DishTypeCreateView.as_view(),
-         name="dish-type-create"),
-    path("dish_type/<int:pk>/update/",
-         DishTypeUpdateView.as_view,
-         name="dish-type-update"),
-    path("dish_type/<int:pk>/delete/",
-         DishTypeDeleteView.as_view,
-         name="dish-type-delete"),
+    # path("dish_type/<int:pk>/detail/",
+    #      DishTypeDetailView.as_view(),
+    #      name="dish-type-detail"),
+    # path("dish_type/create/",
+    #      DishTypeCreateView.as_view(),
+    #      name="dish-type-create"),
+    # path("dish_type/<int:pk>/update/",
+    #      DishTypeUpdateView.as_view,
+    #      name="dish-type-update"),
+    # path("dish_type/<int:pk>/delete/",
+    #      DishTypeDeleteView.as_view,
+    #      name="dish-type-delete"),
 ]
 
 app_name = "kitchen"
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from kitchen.models import Dish, DishType, Cook
 
 
-@login_required
+# @login_required
 def index(request):
     num_dish = Dish.objects.count()
     num_type_dish = DishType.objects.count()
@@ -20,7 +21,30 @@ def index(request):
     return render(request, "kitchen/index.html", context=context)
 
 
-class DishTypeListView(LoginRequiredMixin, generic.ListView):
+class DishTypeListView(generic.ListView):
     model = DishType
     template_name = "kitchen/dish_type/dish_type_list.html"
     paginate_by = 5
+
+
+class DishTypeDetailView(generic.DetailView):
+    model = DishType
+    paginate_by = 5
+    template_name = "kitchen/dish_type/dish_type_detail.html"
+
+
+class DishTypeCreateView(generic.CreateView):
+    model = DishType
+    template_name = "kitchen/dish_type/dish_type_form.html"
+    success_url = reverse_lazy("kitchen/dish_type:dish_type_list")
+
+
+class DishTypeUpdateView(generic.UpdateView):
+    model = DishType
+    template_name = "kitchen/dish_type/dish_type_form.html"
+    success_url = reverse_lazy("kitchen/dish_type:dish_type_list")
+
+
+class DishTypeDeleteView(generic.DeleteView):
+    model = DishType
+    success_url = reverse_lazy("kitchen/dish_type:dish_type_list")
