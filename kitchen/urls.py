73222411580a60d772ec6314e1265a6dc6a9b23a
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2023-06-04 17:01:55 +0300

    In url.py add path to dish-type-CRUD, create dishtype-list-view, dishtype-list-html

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,9 +1,31 @@
 from django.urls import path
 
-from .views import index
+from .views import (
+    index,
+    DishTypeListView,
+    DishTypeDetailView,
+    DishTypeCreateView,
+    DishTypeUpdateView,
+    DishTypeDeleteView,
+)
 
 urlpatterns = [
     path("", index, name="index"),
+    path("dish_type/",
+         DishTypeListView.as_view(),
+         name="dish-type-list"),
+    path("dish_type/<int:pk>/detail/",
+         DishTypeDetailView.as_view(),
+         name="dish-type-detail"),
+    path("dish_type/create/",
+         DishTypeCreateView.as_view(),
+         name="dish-type-create"),
+    path("dish_type/<int:pk>/update/",
+         DishTypeUpdateView.as_view,
+         name="dish-type-update"),
+    path("dish_type/<int:pk>/delete/",
+         DishTypeDeleteView.as_view,
+         name="dish-type-delete"),
 ]
 
 app_name = "kitchen"
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views import generic
 
 from kitchen.models import Dish, DishType, Cook
 
 
 @login_required
 def index(request):
-
     num_dish = Dish.objects.count()
     num_type_dish = DishType.objects.count()
     num_cooks = Cook.objects.count()
@@ -16,4 +17,10 @@ def index(request):
         "num_type_dish": num_type_dish,
         "num_cooks": num_cooks
     }
-    return render(request, "kitchen/index.html", context= context)
+    return render(request, "kitchen/index.html", context=context)
+
+
+class DishTypeListView(LoginRequiredMixin, generic.ListView):
+    model = DishType
+    template_name = "kitchen/dish_type/dish_type_list.html"
+    paginate_by = 5
