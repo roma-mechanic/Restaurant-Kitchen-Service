f7b9be16e539f6881a8456822a8fa364aae38df6
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2023-06-13 16:13:49 +0300

    Add contex processors and includes/footer, includes/navigation, includes/scripts

--- Restaurant_Kitchen_Service/settings.py
+++ Restaurant_Kitchen_Service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,6 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-pwsvl#uoq(_4t1cn0se$@(d82mjdgxj#pqz*p)y3l=38p^&#^#"
+
+# Assets Management
+ASSETS_ROOT = '/static/assets'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,6 +81,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                'kitchen.context_processors.cfg_assets_root',
             ],
         },
     },
